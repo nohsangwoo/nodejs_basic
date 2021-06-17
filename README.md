09b713a01c2a79b717965e2649e7a2786eb1532d
nohsangwoo <nsgr12@naver.com>
2021-06-17 12:06:07 +0900

    sendFile on res

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ToDoList (Coding Apple lecture)
 
+- node를 이용한 간단한 todo List를 구현하며 리마인드
+
+## use
+
+- express, nodemon
+
 ## express 이용 서버 생성
+
+## get request의 response 처리
+
+## 요청에대한 응답으로 파일보내기
--- server.js
+++ server.js
@@ -14,3 +14,10 @@ app.get('/pet', (req, res) => {
 app.get('/beauty', (req, res) => {
   res.send('뷰티 용품 쇼핑 사이트');
 });
+
+// 요청에대한 응답으로 파일을 보낼수 있음
+// __dirname : 현재 디렉토리 경로
+app.get('/', (req, res) => {
+  // __dirname :
+  res.sendFile(__dirname + '/index.html');
+});
